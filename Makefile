ce7e2cfda91bf2f72fdfe11c34effc08ba4d7abc
takahiro <takapi854@gmail.com>
2023-03-24 21:46:45 +0900

    udpate: load env, run api server

---
+++ Makefile
@@ -0,0 +1,5 @@
+fmt:
+	cargo fmt
+
+run: fmt
+	cargo run
--- driver/src/startup/mod.rs
+++ driver/src/startup/mod.rs
@@ -1,14 +1,15 @@
 use crate::routes::health::hc;
 use axum::{routing::get, Router};
 use dotenv::dotenv;
-use std::net::SocketAddr;
+use std::env;
+use std::net::{IpAddr, SocketAddr};
 
 pub async fn startup() {
     let hc_router = Router::new().route("/", get(hc));
 
     let app = Router::new().nest("/hc", hc_router);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let addr = SocketAddr::from(init_addr());
 
     tracing::info!("Server listening on {}", addr);
 
@@ -21,4 +22,23 @@ pub async fn startup() {
 pub fn init_app() {
     dotenv().ok();
     tracing_subscriber::fmt::init();
+}
+
+fn init_addr() -> (IpAddr, u16) {
+    let env_host = env::var_os("HOST").expect("HOST is undefined.");
+    let ip_addr = env_host
+        .into_string()
+        .expect("HOST is invalid.")
+        .parse::<IpAddr>()
+        .expect("HOST is invalid.");
+
+    let env_port = env::var_os("PORT").expect("PORT is undefined.");
+    let port = env_port
+        .into_string()
+        .expect("PORT is invalid.")
+        .parse::<u16>()
+        .expect("PORT is invalid.");
+
+    tracing::debug!("Init ip address.");
+    (ip_addr, port)
 }
