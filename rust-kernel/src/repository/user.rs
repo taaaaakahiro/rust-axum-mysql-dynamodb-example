3d3bd8631353159a11bb87d750de82ce3849d1eb
takahiro <takapi854@gmail.com>
2023-12-03 17:36:52 +0900

    fix user repo

--- rust-kernel/src/repository/user.rs
+++ rust-kernel/src/repository/user.rs
@@ -5,5 +5,4 @@ use crate::model::user::User;
 #[async_trait]
 pub trait UserRepository {
     async fn find_one(&self, id: &String) -> anyhow::Result<Option<User>>;
-    async fn find(&self) -> anyhow::Result<Vec<User>>;
 }
