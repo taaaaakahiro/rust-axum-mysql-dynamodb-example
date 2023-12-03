e4876acaf7ec2ee719ed84ddcb01a142337b0865
takahiro <takapi854@gmail.com>
2023-12-03 17:57:31 +0900

    fix: sql

--- mock/mysql/1_init.sql
+++ mock/mysql/1_init.sql
@@ -4,3 +4,14 @@ CREATE TABLE IF NOT EXISTS `users`(
     `created_at` TIMESTAMP DEFAULT NOW() NOT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `todos`(
+    `id` VARCHAR(20) NOT NULL,
+    `user_id` VARCHAR(20) NOT NULL,
+    `name` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT NOW() NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_todos_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`id`)
+);
--- mock/mysql/2_seed.sql
+++ mock/mysql/2_seed.sql
@@ -1,5 +1,11 @@
 INSERT INTO `users` (id, name) VALUES
-    ("1", "user1"),
-    ("2", "user2"),
-    ("3", "user3"),
-    ("4", "user4");
+("userId1", "userName1"),
+("userId2", "userName2"),
+("userId3", "userName3"),
+("userId4", "userName4");
+
+INSERT INTO `todos` (id, user_id, name) VALUES
+("todoId1", "userId1", "todoName1"),
+("todoId2", "userId2", "todoName2"),
+("todoId3", "userId3", "todoName3"),
+("todoId4", "userId4", "todoName4");
