d3feba527bb38f016f66f1e1ead3003b5bd39f63
takahiro <takapi854@gmail.com>
2023-12-03 19:22:54 +0900

    fix: repo

--- rust-kernel/src/repository/user.rs
+++ rust-kernel/src/repository/user.rs
@@ -6,4 +6,5 @@ use crate::model::user::User;
 pub trait UserRepository {
     async fn find_one(&self, id: &String) -> anyhow::Result<Option<User>>;
     async fn find(&self) -> anyhow::Result<Vec<User>>;
+    async fn delete(&self, id: &String) -> anyhow::Result<String>;
 }
