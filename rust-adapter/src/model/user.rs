66cad49af46a0c5e9ec46404e923c2f4fc645d4a
takahiro <takapi854@gmail.com>
2023-12-07 22:29:31 +0900

    add insert repo

--- rust-adapter/src/model/user.rs
+++ rust-adapter/src/model/user.rs
@@ -1,17 +1,34 @@
 use chrono::{DateTime, Local};
-use kernel::model::user::User;
+use kernel::model::user::{NewUser, User};
 use sqlx::FromRow;
 
-#[derive(FromRow)]
+#[derive(FromRow, Debug)]
 pub struct UserTable {
     pub id: String,
     pub name: String,
     pub created_at: DateTime<Local>,
 }
 
+impl UserTable {
+    pub fn id(&self) -> String {
+        self.id.to_string()
+    }
+}
+
 impl TryFrom<UserTable> for User {
     type Error = anyhow::Error;
-    fn try_from(st: UserTable) -> Result<Self, Self::Error> {
-        Ok(User::new(st.id, st.name, st.created_at))
+    fn try_from(ut: UserTable) -> Result<Self, Self::Error> {
+        Ok(User::new(ut.id, ut.name, ut.created_at))
+    }
+}
+
+impl TryFrom<NewUser> for UserTable {
+    type Error = anyhow::Error;
+    fn try_from(nu: NewUser) -> Result<Self, Self::Error> {
+        Ok(UserTable {
+            id: nu.id,
+            name: nu.name,
+            created_at: Local::now(),
+        })
     }
 }
