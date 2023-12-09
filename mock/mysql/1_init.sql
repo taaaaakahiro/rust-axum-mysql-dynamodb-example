8754951fa081f62ca88c017898b5dc00eb4cdec0
takahiro <takapi854@gmail.com>
2023-12-09 16:57:32 +0900

    fix

--- mock/mysql/1_init.sql
+++ mock/mysql/1_init.sql
@@ -5,13 +5,13 @@ CREATE TABLE IF NOT EXISTS `users`(
     PRIMARY KEY (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `todos`(
-    `id` VARCHAR(20) NOT NULL,
-    `user_id` VARCHAR(20) NOT NULL,
-    `name` VARCHAR(50) NOT NULL,
-    `created_at` TIMESTAMP DEFAULT NOW() NOT NULL,
-    PRIMARY KEY (`id`),
-    CONSTRAINT `fk_todos_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `users` (`id`)
-);
+-- CREATE TABLE IF NOT EXISTS `todos`(
+--     `id` VARCHAR(20) NOT NULL,
+--     `user_id` VARCHAR(20) NOT NULL,
+--     `name` VARCHAR(50) NOT NULL,
+--     `created_at` TIMESTAMP DEFAULT NOW() NOT NULL,
+--     PRIMARY KEY (`id`),
+--     CONSTRAINT `fk_todos_user1`
+--     FOREIGN KEY (`user_id`)
+--     REFERENCES `users` (`id`)
+-- );
--- mock/mysql/2_seed.sql
+++ mock/mysql/2_seed.sql
@@ -4,8 +4,8 @@ INSERT INTO `users` (id, name) VALUES
 ("userId3", "userName3"),
 ("userId4", "userName4");
 
-INSERT INTO `todos` (id, user_id, name) VALUES
-("todoId1", "userId1", "todoName1"),
-("todoId2", "userId2", "todoName2"),
-("todoId3", "userId3", "todoName3"),
-("todoId4", "userId4", "todoName4");
+-- INSERT INTO `todos` (id, user_id, name) VALUES
+-- ("todoId1", "userId1", "todoName1"),
+-- ("todoId2", "userId2", "todoName2"),
+-- ("todoId3", "userId3", "todoName3"),
+-- ("todoId4", "userId4", "todoName4");
